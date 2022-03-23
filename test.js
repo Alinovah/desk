b1fe129867fe8b7949b2e33b6976b8cfd1000c6f
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-03-23 11:35:13 +0200

    Update test.js

--- test.js
+++ test.js
@@ -17,7 +17,35 @@ if (currentlink.includes("https://desk.ngsub.tv/vendors/")) {
       let ui = await ui_res.json();
       ui_G = ui;
       console.log(ui);
-      // start(ui);
+      start(ui);
     }
   }, 500);
+}
+
+async function start(ui) {
+  if (ui == "Standard") {
+  } else if (ui == "Netflix") {
+    let start_interval = setInterval(function () {
+      let location = window.location.href;
+      //############### check if inside job #################
+      let inside_job_location = "https://desk.ngsub.tv/vendors/#/jobs/";
+      if (location.includes(inside_job_location)) {
+        //### check if job is netflix ###
+        let job_type = document.querySelector(
+          'dd[class="job-type ng-binding"]'
+        ).innerText;
+        if (job_type == "Netflix") {
+          //### change text of submit buttons ###
+          let partial_finished_button = document.querySelector(
+            "span[translate='JOB.PARTLY_FINISH']"
+          );
+          partial_finished_button.textContent = "Save Uploaded Files";
+          let finished_button = document.querySelector(
+            "span[translate='JOB.FINISH']"
+          );
+          finished_button.textContent = "Confirm Job";
+        }
+      }
+    }, 1000);
+  }
 }
