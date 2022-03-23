691ef1ead476fdae2a768a410ce464f6e42b0c52
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-03-23 11:16:42 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-console.log("External Script Loaded")
+console.log("External Script Loaded");
 var ui_G = "";
 var currentlink = window.location.href;
 console.log(currentlink);
 //####################### vendor portal #########################
-if (currentlink.indexOd("https://desk.ngsub.tv/vendors/")) {
+if (currentlink.includes("https://desk.ngsub.tv/vendors/")) {
   console.log("vendor portal");
 }
