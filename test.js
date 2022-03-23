a882f77fbccd38bba37226ec9faaefe0c8e6737c
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-03-23 11:14:24 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 console.log("Apache Insertion!")
+var ui_G = "";
+var currentlink = window.location.href;
+console.log(currentlink);
+//####################### vendor portal #########################
+if (currentlink.indexOd("https://desk.ngsub.tv/vendors/")) {
+  console.log("vendor portal");
+}
