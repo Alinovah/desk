8a0234e45b6e5dd16356af97ec501c1ea0c277cf
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-03-23 11:24:45 +0200

    Update test.js

--- test.js
+++ test.js
@@ -5,4 +5,19 @@ console.log(currentlink);
 //####################### vendor portal #########################
 if (currentlink.includes("https://desk.ngsub.tv/vendors/")) {
   console.log("vendor portal");
+  //####################### get the ui #########################
+  let getui = setInterval(async function () {
+    let email_container = document.querySelector("span[class*='email']");
+    if (email_container) {
+      clearInterval(getui);
+      let email = email_container.innerText;
+      let ui_res = await fetch(
+        "https://xtrfsubscriptions.ngsub.tv:7898/getui?id=" + email
+      );
+      let ui = await ui_res.json();
+      ui_G = ui;
+      console.log(ui);
+      // start(ui);
+    }
+  }, 500);
 }
