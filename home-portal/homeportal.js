2eea16beb077d3bc18fd817c8fc7f5bf6f31694b
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-03-29 14:44:45 +0300

    Update homeportal.js

--- home-portal/homeportal.js
+++ home-portal/homeportal.js
@@ -1,41 +1,10 @@
 
+//////////////////////// generate selfinvoice ////////////////////////////////
 (()=>{let home_portal_script = document.createElement("script");
 home_portal_script.type = "text/javascript";
-home_portal_script.src = "https://alinovah.github.io/desk/home-portal/test.js";
+home_portal_script.src = "https://alinovah.github.io/desk/home-portal/selfinvoice.js";
 document.querySelector("body").appendChild(home_portal_script);})();
 
-
-//////////////////////// generate masspayment ////////////////////////////////
-  if(location.href.includes("https://desk.ngsub.tv/xtrf/faces/providerInvoices/browse.seam")){
-  let createselfinvoice_interval = setInterval(function() {
-    let button = document.createElement("button");
-    let div = document.querySelector('div[class="x-title-bar__actions"]');
-    if(!div){
-    }else{
-    clearInterval(createselfinvoice_interval);
-    button.innerText = "Self Invoice";
-    button.className = "dropdown-toggle x-btn --large ng-scope"
-    button.style.marginLeft = "10px"
-    button.onclick=()=>{createselfinvoice()};
-    div.appendChild(button);
-    }
-    }, 500);
-}
-
-
-async function createselfinvoice(){
-  let buffer_res = await fetch("https://xtrfsubscriptions.ngsub.tv:7903/createselfinvoice");
-  let buffer = await buffer_res.text();
-  downloadBase64File("aapplication/vnd.openxmlformats-officedocument.spreadsheetml.sheet",buffer,"SelfInvoice.xlsx");
-}
-
-function downloadBase64File(contentType, base64Data, fileName) {
-   const linkSource = `data:${contentType};base64,${base64Data}`;
-   const downloadLink = document.createElement("a");
-   downloadLink.href = linkSource;
-   downloadLink.download = fileName;
-   downloadLink.click();
-}
 
 //////////////////////// Duplicate Project Handler //////////////////////////
 if (location.href.includes("https://desk.ngsub.tv/xtrf/faces/project/browse.seam")) {
