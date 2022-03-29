4bc3a71167273dcb56041b7bb4a1f36d86f5953d
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-03-29 15:06:35 +0300

    Update homeportal.js

--- home-portal/homeportal.js
+++ home-portal/homeportal.js
@@ -2,11 +2,11 @@
 //////////////////////// generate selfinvoice ////////////////////////////////
 (()=>{let home_portal_script = document.createElement("script");
 home_portal_script.type = "text/javascript";
-home_portal_script.src = "https://alinovah.github.io/desk/home-portal/selfinvoice.js";
+home_portal_script.src = "https://alinovah.github.io/desk/home-portal/self_invoice.js";
 document.querySelector("body").appendChild(home_portal_script);})();
 
 //////////////////////// duplicate project //////////////////////////
 (()=>{let home_portal_script = document.createElement("script");
 home_portal_script.type = "text/javascript";
-home_portal_script.src = "https://alinovah.github.io/desk/home-portal/duplicateproject.js";
+home_portal_script.src = "https://alinovah.github.io/desk/home-portal/duplicate_project.js";
 document.querySelector("body").appendChild(home_portal_script);})();
