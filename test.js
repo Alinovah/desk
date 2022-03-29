71507cc049f78bdcb0763bd104cc11d136d90fc2
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-03-29 14:18:17 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 console.log("External Script Loaded");
-let currentlink = window.location.href;
-console.log(currentlink);
+let currentlink_G = window.location.href;
+console.log(currentlink_G);
 
 let script = document.createElement("script");
 script.type = "text/javascript";
 
 //### home portal ###
-if (currentlink.includes("https://desk.ngsub.tv/xtrf/faces/")) {
+if (currentlink_G.includes("https://desk.ngsub.tv/xtrf/faces/")) {
   console.log("home portal");
   script.src = "https://alinovah.github.io/desk/homeportal.js";
   document.querySelector("body").appendChild(script);
 }
 //### vendor portal ###
-if (currentlink.includes("https://desk.ngsub.tv/vendors/")) {
+if (currentlink_G.includes("https://desk.ngsub.tv/vendors/")) {
   console.log("vendor portal");
   script.src = "https://alinovah.github.io/desk/vendorsportal.js";
   document.querySelector("body").appendChild(script);
