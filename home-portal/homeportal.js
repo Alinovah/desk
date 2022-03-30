2a9a9ecb480b6321a93b33b60f0a5e3809c8ea72
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-03-30 09:36:23 +0300

    Update homeportal.js

--- home-portal/homeportal.js
+++ home-portal/homeportal.js
@@ -8,7 +8,7 @@ document.querySelector("body").appendChild(home_portal_script);})();
 //////////////////////// duplicate project //////////////////////////
 (()=>{let home_portal_script = document.createElement("script");
 home_portal_script.type = "text/javascript";
-home_portal_script.src = "https://alinovah.github.io/desk/home-portal/duplicate_project.js";
+home_portal_script.src = "https://alinovah.github.io/desk/home-portal/project/duplicate_project.js";
 document.querySelector("body").appendChild(home_portal_script);})();
 
 ////////////////////////// fix stuck after receivables quote or project /////////////////////
