c878f6b3366bb72bb700be67bd561bdf1aa2b0bb
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-03-30 15:50:36 +0300

    Update add_quote_management_view.js

--- home-portal/quote/add_quote_management_view.js
+++ home-portal/quote/add_quote_management_view.js
@@ -1,4 +1,4 @@
-if(window.location.href.includes("https://desk.ngsub.tv/xtrf/faces/quote/browse.seam")>-1){
+if(window.location.href.includes("https://desk.ngsub.tv/xtrf/faces/quote/browse.seam")){
       let quoteview_interval = setInterval(function() {
       let ul = document.querySelectorAll("ul")[6];
       if(ul){
@@ -12,12 +12,35 @@ if(window.location.href.includes("https://desk.ngsub.tv/xtrf/faces/quote/browse.
     }, 500);
 }
 
+if(window.location.href.includes("&quotemgm")){
+  let quoteview_interval_reload = setInterval(function() {
+    let ul = document.querySelectorAll("ul")[6];
+    if(ul){
+    clearInterval(quoteview_interval_reload);
+    let lis = ul.querySelectorAll("li");
+    lis.forEach((li)=>{
+      if(li.innerHTML=="Quote Management"){
+        li.classList.add("active");
+        li.style.paddingTop = "3px";
+      }else{
+        li.classList.remove("active");
+      }
+    });
+    document.querySelector("h1").remove();
+    let content = document.querySelector("div[class='content']");
+    let pm = document.querySelector('div[class="name"]').textContent;
+    content.innerHTML = '<iframe style="position:fixed;border:0;" width="95%" height="87%" src="https://xtrfsubscriptions.ngsub.tv:7903/quote?pm='+pm+'" title="overallview"></iframe>';
+    }
+  }, 500);
+}
+
 function showQuoteManagement(target){
   let lis = target.parentElement.querySelectorAll("li");
   lis.forEach((li)=>{
     li.classList.remove("active");
   });
   target.classList.add("active");
+  window.location.href = window.location.href + "&quotemgm";
   target.style.padding = "3px";
   document.querySelector("h1").remove();
   let content = document.querySelector("div[class='content']");
