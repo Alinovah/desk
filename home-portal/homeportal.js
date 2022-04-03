1f65ea48affed4c022c084b59b7dfc299a99a3e5
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-04-03 09:28:11 +0300

    Update homeportal.js

--- home-portal/homeportal.js
+++ home-portal/homeportal.js
@@ -1,10 +1,23 @@
 (()=>{
+  //###invoices
   appendScript("https://alinovah.github.io/desk/home-portal/invoice/self_invoice.js");
-  appendScript("https://alinovah.github.io/desk/home-portal/project/duplicate_project.js");
-  appendScript("https://alinovah.github.io/desk/home-portal/project_and_quote/fix_stuck_after_receivable.js");
-  appendScript("https://alinovah.github.io/desk/home-portal/customer/color_red.js");
+  
+  //###quotes
   appendScript("https://alinovah.github.io/desk/home-portal/quote/add_quote_management_view.js");
+  
+  //###projects
+  appendScript("https://alinovah.github.io/desk/home-portal/project/duplicate_project.js");
+  
+  //###projects and quotes
+  appendScript("https://alinovah.github.io/desk/home-portal/project_and_quote/loader.js");
+  appendScript("https://alinovah.github.io/desk/home-portal/project_and_quote/fix_stuck_after_receivable.js");
+  
+  //###customers
+  appendScript("https://alinovah.github.io/desk/home-portal/customer/color_red.js");
+  
+  //###reports
   appendScript("https://alinovah.github.io/desk/home-portal/report/add_margin_view.js");
+  
 })();
 
 /////////////////////// append script //////////////////////
