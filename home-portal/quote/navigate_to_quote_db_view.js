db63f39a007856001b32e34c3453fd2e241f2560
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-04-07 16:58:21 +0300

    Update navigate_to_quote_db_view.js

--- home-portal/quote/navigate_to_quote_db_view.js
+++ home-portal/quote/navigate_to_quote_db_view.js
@@ -1,1 +1,19 @@
-
+if(window.location.href.substring(0,67)=="https://desk.ngsub.tv/xtrf/faces/projectAssistant/quotes/quote.seam"){
+let interval_redirect_quote_db = setInterval(function() {
+    if($('[class^="input__item"]').length>=34){
+        clearInterval(interval_redirect_quote_db);
+        let input_items = document.querySelectorAll("div[class='input__item']");
+        let service;
+        input_items.forEach((div)=>{
+          if(div.querySelector("div").innerText=="Service"){
+            service = div.querySelectorAll("div")[1].innerText;
+          }
+        });
+        if(service=="DB Quote"){
+          let pid = window.assistedProjectId;
+          let pm = document.querySelector("div[class='name']").textContent;
+          window.location.href = `https://xtrfsubscriptions.ngsub.tv:7903/showquote?pid=${pid}&pm=${pm}` ;
+        }
+    }
+},500);
+}
