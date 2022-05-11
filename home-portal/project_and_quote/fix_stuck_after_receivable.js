3610b9f6df3b6855a05dbfa866321c48f22d8ba6
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-05-11 15:32:03 +0300

    Update fix_stuck_after_receivable.js

--- home-portal/project_and_quote/fix_stuck_after_receivable.js
+++ home-portal/project_and_quote/fix_stuck_after_receivable.js
@@ -4,7 +4,13 @@ if(window.location.href.substring(window.location.href.length-63,window.location
   console.log(redirect);
   window.location = redirect;
 }
-if(window.location.href.substring(window.location.href.length-66,window.location.href.length)=="paFinancePanel%3AformWithPanels%3AreceivablesTab&action=#/project"){ 
+if(window.location.href.substring(window.location.href.length-65,window.location.href.length)=="paFinancePanel%3AformWithPanels%3AreceivablesTab&action=#/project"){ 
+   console.log("bad url");
+   var redirect = window.location.href.substring(0,window.location.href.length-71);
+   console.log(redirect);
+   window.location = redirect;
+}   
+if(window.location.href.substring(window.location.href.length-62,window.location.href.length)=="paFinancePanel%3AformWithPanels%3AreceivablesTab&action=#/project"){ 
    console.log("bad url");
    var redirect = window.location.href.substring(0,window.location.href.length-71);
    console.log(redirect);
