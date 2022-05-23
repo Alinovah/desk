692c693747109b670698149d5035c774d9ff74cf
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-05-23 08:55:10 +0300

    Update homeportal.js

--- home-portal/homeportal.js
+++ home-portal/homeportal.js
@@ -18,6 +18,7 @@ const BASE_URL = "https://alinovah.github.io/desk/home-portal/";
   appendScript(`${BASE_URL}project_and_quote/fix_stuck_after_receivable.js`);
   appendScript(`${BASE_URL}project_and_quote/color_gantt.js`);
   appendScript(`${BASE_URL}project_and_quote/rearrange_view.js`);
+  appendScript(`${BASE_URL}project_and_quote/rearrange_widgets.js`);
   
   //###customers
   appendScript(`${BASE_URL}customer/color_red.js`);
