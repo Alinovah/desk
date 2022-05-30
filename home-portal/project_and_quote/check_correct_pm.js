254d7a96074ac78a6cdc73fb81c150204d8e502f
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-05-30 15:47:43 +0300

    Update check_correct_pm.js

--- home-portal/project_and_quote/check_correct_pm.js
+++ home-portal/project_and_quote/check_correct_pm.js
@@ -21,11 +21,13 @@ if (
         var name = document.querySelector("div[class='name']").textContent;
         if (!(pm_name == name)) {
           if(document.querySelector(".email").textContent=='dani.berger@ngsub.tv'){
+            console.log("redirect");
             window.location.href = "https://desk.ngsub.tv/xtrf/faces/dashboard2/dashboard.seam"
+            }else{
+            alert(
+              `The PM set for this project is ${pm_name} and the logged in user is ${name}.\n If the project is yours, change the PM at "People".`
+            );
           }
-          alert(
-            `The PM set for this project is ${pm_name} and the logged in user is ${name}.\n If the project is yours, change the PM at "People".`
-          );
         }
 //       }, 3000);
     }
