fae398bdb09e72f069d59a52bce782181228423d
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-05-30 09:27:11 +0300

    Update homeportal.js

--- home-portal/homeportal.js
+++ home-portal/homeportal.js
@@ -25,6 +25,7 @@ const BASE_URL = "https://alinovah.github.io/desk/home-portal/";
   appendScript(`${BASE_URL}project_and_quote/change_raw_materials_volume_field_name.js`);
   appendScript(`${BASE_URL}project_and_quote/rtl_textarea.js`);
   appendScript(`${BASE_URL}project_and_quote/color_empty_red.js`);
+  appendScript(`${BASE_URL}project_and_quote/fix_duplicates_after_language_change.js`);
   
   //###customers
   appendScript(`${BASE_URL}customer/color_red.js`);
