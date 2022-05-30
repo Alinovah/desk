7a52d2cfe35723c801bdd59d52f1e466c89c5421
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-05-30 08:47:24 +0300

    Create change_raw_materials_volume_field_name.js

---
+++ home-portal/project_and_quote/change_raw_materials_volume_field_name.js
@@ -0,0 +1,18 @@
+if (
+  currentlink.substring(0, 71) ==
+    "https://desk.ngsub.tv/xtrf/faces/projectAssistant/projects/project.seam" ||
+  currentlink.substring(0, 67) ==
+    "https://desk.ngsub.tv/xtrf/faces/projectAssistant/quotes/quote.seam"
+) {
+  var service = $('[class^="input ng-binding ng-scope"]')[0];
+  var titles = document.querySelectorAll(
+    'div[class="title ng-binding ng-scope"]'
+  );
+  if (service.textContent == "Raw Materials") {
+    for (var i = 0; i < titles.length; i++) {
+      if (titles[i].textContent == "number of video files") {
+        titles[i].textContent = "Number of pages";
+      }
+    }
+  }
+}
