c9f1e4d870273ed937c538878449932337eb9e4f
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-05-30 16:14:48 +0300

    Update overallview.js

--- home-portal/project/overallview.js
+++ home-portal/project/overallview.js
@@ -1,8 +1,13 @@
 if(window.location.href.indexOf("https://desk.ngsub.tv/xtrf/faces/workflowResource/browse.seam")>-1){
-
+    let interval = setInterval(()=>{
+        if(window.location.href.includes("overallview")){
+            clearInterval(interval);
+        }else{
             document.querySelector("h1").remove();
             let content = document.querySelector("div[class='content']");
             let pm = document.querySelector('div[class="name"]').textContent;
             content.innerHTML = '<iframe style="position:fixed;" width="95%" height="87%" src="https://xtrfsubscriptions.ngsub.tv:7903/overallview?pm='+pm+'" title="overallview"></iframe>';
-
+            window.history.pushState(null,null,window.location.href+"?overallview");
+        }
+    },500);
 }
