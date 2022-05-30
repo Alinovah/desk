59b97c7582112fbdccce0d3f0bf87883ad00b627
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-05-30 09:17:11 +0300

    Update rtl_textarea.js

--- home-portal/project_and_quote/rtl_textarea.js
+++ home-portal/project_and_quote/rtl_textarea.js
@@ -4,22 +4,27 @@ if (
   window.location.href.substring(0, 67) ==
     "https://desk.ngsub.tv/xtrf/faces/projectAssistant/quotes/quote.seam"
 ) {
-  if (
-    document.querySelector('textarea[ng-model="order.customerNotes"]') &&
-    document.querySelector('textarea[ng-model="order.internalNotes"]')
-  ) {
-    document.querySelector('textarea[ng-model="order.customerNotes"]').dir =
-      "rtl";
-    document.querySelector('textarea[ng-model="order.internalNotes"]').dir =
-      "rtl";
-  }
-  if (
-    document.querySelector('textarea[ng-model="quote.customerNotes"]') &&
-    document.querySelector('textarea[ng-model="quote.internalNotes"]')
-  ) {
-    document.querySelector('textarea[ng-model="quote.customerNotes"]').dir =
-      "rtl";
-    document.querySelector('textarea[ng-model="quote.internalNotes"]').dir =
-      "rtl";
-  }
+  let interval = setInterval(function () {
+        if ($('[class^="input__item"]').length >= 34) {
+            clearInterval(interval);
+            if (
+              document.querySelector('textarea[ng-model="order.customerNotes"]') &&
+              document.querySelector('textarea[ng-model="order.internalNotes"]')
+            ) {
+              document.querySelector('textarea[ng-model="order.customerNotes"]').dir =
+                "rtl";
+              document.querySelector('textarea[ng-model="order.internalNotes"]').dir =
+                "rtl";
+            }
+            if (
+              document.querySelector('textarea[ng-model="quote.customerNotes"]') &&
+              document.querySelector('textarea[ng-model="quote.internalNotes"]')
+            ) {
+              document.querySelector('textarea[ng-model="quote.customerNotes"]').dir =
+                "rtl";
+              document.querySelector('textarea[ng-model="quote.internalNotes"]').dir =
+                "rtl";
+            }
+        }
+  },1000);
 }
