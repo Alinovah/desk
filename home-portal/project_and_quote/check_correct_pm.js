54417790178b86dece624692dfa9a5cfc24bdd48
Alinovah <89099080+Alinovah@users.noreply.github.com>
2022-05-30 09:29:57 +0300

    Create check_correct_pm.js

---
+++ home-portal/project_and_quote/check_correct_pm.js
@@ -0,0 +1,30 @@
+if (
+  window.location.href.substring(0, 71) ==
+    "https://desk.ngsub.tv/xtrf/faces/projectAssistant/projects/project.seam" ||
+  window.location.href.substring(0, 67) ==
+    "https://desk.ngsub.tv/xtrf/faces/projectAssistant/quotes/quote.seam"
+) {
+  let interval = setInterval(function () {
+    if ($('[class^="input__item"]').length >= 34) {
+      clearInterval(interval);
+
+      setTimeout(function () {
+        var inputs = document.querySelectorAll("div[class='input__item']");
+        var pm = "";
+        for (var i = 0; i < inputs.length; i++) {
+          var title = inputs[i].querySelector("div").innerText;
+          if (title == "Project Manager") {
+            pm = inputs[i].querySelectorAll("div")[1];
+          }
+        }
+        var pm_name = pm.querySelector("a").innerText;
+        var name = document.querySelector("div[class='name']").textContent;
+        if (!(pm_name == name)) {
+          alert(
+            `The PM set for this project is ${pm_name} and the logged in user is ${name}.\n If the project is yours, change the PM at "People".`
+          );
+        }
+      }, 3000);
+    }
+  }, 100);
+}
